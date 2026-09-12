1b1db2e57a43f4fc3bb996489a3327141df326d4
PRAVEEN M <pm9693@srmist.edu.in>
2026-09-12 22:27:51 +0530

    S6_Class_M5: Add Student College Information Code

---
+++ src/main/java/S6_ObjectModelling/class_problems/M5StudentCollegeInfo.java
@@ -0,0 +1,33 @@
+package S6_ObjectModelling.class_problems;
+
+class Student {
+    String name;
+    double attendance;
+
+    static String collegeName =
+            "SRM Institute of Science and Technology";
+
+    static int studentCount = 0;
+
+    Student(String name, double attendance) {
+        this.name = name;
+        this.attendance = attendance;
+
+        studentCount++;
+    }
+
+    static void printCollegeInfo() {
+        System.out.println(collegeName);
+        System.out.println("Students created: " + studentCount);
+    }
+}
+
+public class M5StudentCollegeInfo{
+    public static void main(String[] args) {
+
+        Student s1 = new Student("Ravi", 85.5);
+        Student s2 = new Student("Anitha", 90.0);
+
+        Student.printCollegeInfo();
+    }
+}
