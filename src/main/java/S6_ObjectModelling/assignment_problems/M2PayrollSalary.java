8bf13aa76ff74466696615688675955a66834b4d
PRAVEEN M <pm9693@srmist.edu.in>
2026-09-12 22:26:56 +0530

    S6_Assignment_M2: Add Payroll Salary Management Code

---
+++ src/main/java/S6_ObjectModelling/assignment_problems/M2PayrollSalary.java
@@ -0,0 +1,52 @@
+package S6_ObjectModelling.assignment_problems;
+
+class PayrollAccount {
+    private double basicSalary;
+    private double bonus;
+
+    public PayrollAccount(double basicSalary) {
+        if (basicSalary < 0) {
+            System.out.println("Negative basic salary. Starting at Rs 0.0");
+            this.basicSalary = 0;
+        } else {
+            this.basicSalary = basicSalary;
+        }
+
+        this.bonus = 0;
+    }
+
+    public void creditBonus(double amount) {
+        if (amount <= 0) {
+            System.out.println("Bonus rejected: amount must be positive");
+        } else {
+            bonus += amount;
+            System.out.println("Bonus credited: Rs " + amount);
+        }
+    }
+
+    public void deductTax(double percent) {
+        if (percent < 0 || percent > 100) {
+            System.out.println("Tax rejected: percent must be between 0 and 100");
+        } else {
+            basicSalary -= basicSalary * percent / 100;
+            System.out.println("Tax deducted: " + percent + "%");
+        }
+    }
+
+    public double getNetSalary() {
+        return basicSalary + bonus;
+    }
+}
+
+public class M2PayrollSalary {
+    public static void main(String[] args) {
+
+        PayrollAccount account = new PayrollAccount(50000);
+
+        account.creditBonus(5000);
+        account.deductTax(10);
+
+        System.out.println("Net salary: Rs " + account.getNetSalary());
+    }
+}
+
