a519c792df88460a523cc1c77b1f095db2382794
PRAVEEN M <pm9693@srmist.edu.in>
2026-09-12 22:27:01 +0530

    S6_Assignment_M3: Add Employee Profile Management Code

---
+++ src/main/java/S6_ObjectModelling/assignment_problems/M3EmployeeProfile.java
@@ -0,0 +1,40 @@
+package S6_ObjectModelling.assignment_problems;
+
+class Employee {
+    String empId;
+    String empName;
+    double salary;
+    boolean isIntern;
+
+    public Employee(String empId, String empName, double salary) {
+        this.empId = empId;
+        this.empName = empName;
+        this.salary = salary;
+        this.isIntern = false;
+    }
+
+    public Employee(String empId, String empName) {
+        this(empId, empName, 0);
+        this.isIntern = true;
+    }
+
+    public void printProfile() {
+        System.out.println(empId + " | " + empName
+                + " | Rs " + salary
+                + " | Intern: " + isIntern);
+    }
+}
+
+public class M3EmployeeProfile {
+    public static void main(String[] args) {
+
+        Employee permanentEmployee =
+                new Employee("E-101", "Divya", 65000);
+
+        Employee internEmployee =
+                new Employee("E-102", "Arjun");
+
+        permanentEmployee.printProfile();
+        internEmployee.printProfile();
+    }
+}
